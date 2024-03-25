d55cf71ccdc9d2b9761f5af2a95495e285f08564
anisyahramadanii <164367635+anisyahramadanii@users.noreply.github.com>
2024-03-25 11:50:11 +0700

    Add files via upload

---
+++ ukk_gallery/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600;700&family=Work+Sans:ital,wght@0,200;0,400;0,700;1,100;1,400;1,600&display=swap"
+ rel="stylesheet">
+
+ <!-- simplyCountdown -->
+ <link rel="stylesheet" href="countdown/simplyCountdown.theme.default.css"/>
+    <script src="countdown/simplyCountdown.min.js"></script>
+
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+ 
+ 
+    <link rel="stylesheet" href="style.css">
+
+	<title>Website Galery Foto</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+</head>
+<body class="bg-light">
+  <nav class="navbar navbar-expand-lg navbar-danger-subtle bg-danger-subtle">
+    <div class="container">
+      <a class="navbar-brand" href="index.php"> Anisa's Gallery </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+        
+      </div>
+      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-primary m-1">Masuk</a>
+    </div>
+  </div>
+</nav>
+
+ <div class="container">
+ <div class="row justify-content-center text-center">
+ <div class="card mb-3">
+  <div class="card text-bg-dark">
+  <img src="assets/img/ass.png" class="card-img" alt="memories">
+  <div class="card-img-overlay">
+   
+</div>
+</div>
+  </div>
+</div>
+
+
+	</div>
+	</div>
+
+
+</body>
+
+
+<footer class="d-flex justify-content-center border-top mt-3 bg-danger-subtle">
+	<p>&copy; UKK RPL 2024 | Anisyah Ramadani</p>
+</footer>
+<script type="text/javascript" src="assets/css/bootstrap.min.js"></script>
+</body>
+</html>
